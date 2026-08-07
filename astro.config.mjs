9dfdc6290ee767d56156f4bcc91240105a9a10c7
KonyD <asilpoyrazsahin@gmail.com>
2026-08-08 00:56:55 +0300

    change url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://asilpoyrazsahin.github.io',
+  site: 'https://asilpoyraz.me/',
   base: '/',
   integrations: [tailwind()]
 });
